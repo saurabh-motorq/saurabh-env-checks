259c93c28bef95c3816bf14a6eccfffa34085f76
saurabh-motorq <saurabh@motorq.com>
2021-04-23 09:57:34 +0530

    PG latest event check

--- postgresdb-checks/index.js
+++ postgresdb-checks/index.js
@@ -1,9 +1,10 @@
 require('dotenv').config();
 const config = require("./config");
 const latestTripCountCheckRepo = require('./latest-trip-count-check-repo');
-
+const latestEventCountCheckRepo = require('./latest-event-count-check-repo')
 module.exports = async function (context, myTimer) {   
-    context = console; 
+    context = console;
     await latestTripCountCheckRepo.performLatestTripCountCheck(context);
+    await latestEventCountCheckRepo.performLatestEventCountCheck(context);
     // context.log('JavaScript timer trigger function ran!', timeStamp);  
 };
---
+++ postgresdb-checks/latest-event-count-check-repo.js
@@ -0,0 +1,28 @@
+var config = require("./config");
+const { environments } = require('../common/environments/index');
+const pgRepo = require('./pg-repo')
+async function performLatestEventCountCheck(context)
+{
+    for(const env of environments){
+        const eventFeed = config.pgTables.eventFeed;
+        var eventQuerySpec = {
+            query: `SELECT count(*) AS count FROM ${eventFeed} WHERE event_timestamp > now() - interval '1 day'`
+        };
+        try{
+            const tripCount = await pgRepo.queryPG(eventQuerySpec.query);
+            context.log(tripCount.rows[0].count);
+            if (tripCount.rows[0].count>0) {
+                    context.log('events found in last 24 hrs');
+            } else {
+                context.log('No event found in the last day');
+                await pgRepo.insertAlertIntoPg(env.name,'EVENT_COUNTS_IN_POSTGRES','no events found in the last 24 hours' );
+            }
+        }
+        catch(err){
+            context.log(err);
+            await pgRepo.insertAlertIntoPg(env.name,'EVENT_COUNTS_IN_POSTGRES', 'Check Failed');
+        }
+    }
+}
+
+module.exports = { performLatestEventCountCheck };
